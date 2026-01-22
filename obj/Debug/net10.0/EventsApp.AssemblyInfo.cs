6c7dc1d69ea3c0dee5e3f7384727fff0fd9ce820
Taylor Sullivan <Cirburus@users.noreply.github.com>
2026-01-22 11:30:11 -0600

    Update .gitignore

--- obj/Debug/net10.0/EventsApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/EventsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3df23892b6c97b3a201686b3e5fe8f78940c693d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
